e1061c681d8a9073cb59f5d3d8f49f9abe0d48f0
Ghita <ghita.fatih0@gmail.com>
2025-11-12 12:07:03 +0100

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, TextInput, Button, ScrollView, StyleSheet,Alert} from 'react-native';
 
-export default function App() {
+export default function ProfilStatique() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image
+        source={{ uri: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131' }}
+        style={styles.avatar}
+      />
+      <Text style={styles.title}>Profil utilisateur</Text>
+      <Text style={styles.label}>Nom :</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nom modifiable"
+        editable={true}
+        value="cat"
+      />
+      <Text style={styles.label}>Message :</Text>
+      <Text style={styles.message}>hello 😊</Text>
+      <Button title="Action activée" onPress={() => {}} enabled />
+        <Text> </Text> 
+        <Button title="Afficher une alerte" onPress={() => {  Alert.alert('Bonjour !', 'Vous avez appuyé sur le bouton 😊'); }} />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  label: {
+    alignSelf: 'flex-start',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+    backgroundColor: '#eee',
+  },
+  message: {
+    fontSize: 18,
+    color: 'gray',
+    marginBottom: 20,
   },
 });
